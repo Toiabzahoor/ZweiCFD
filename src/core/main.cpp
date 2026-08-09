ddd31e15ecd35843b224c9b2f92f0bd0e4612234
Toiab Zahoor <toiabzahoor@gmail.com>
2026-08-09 16:02:54 +0530

    main catches error on fail now

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -2,15 +2,24 @@
 #include <QApplication>
 #include <QSurfaceFormat>
 #include <QVTKOpenGLNativeWidget.h>
+#include <QMessageBox>
+#include <QDebug>
 
 int main(int argc, char* argv[]) {
-    
+
     QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat());
 
     QApplication app(argc, argv);
-    
-    zweicfd::MainWindow window;
-    window.show();
-    
-    return app.exec();
+
+    try {
+        zweicfd::MainWindow window;
+        window.show();
+
+        return app.exec();
+    } catch (const std::exception& e) {
+        qCritical() << "ZweiCFD failed to start:" << e.what();
+        QMessageBox::critical(nullptr, "ZweiCFD failed to start",
+            QString("ZweiCFD ran into a fatal error on startup:\n\n%1").arg(e.what()));
+        return 1;
+    }
 }
